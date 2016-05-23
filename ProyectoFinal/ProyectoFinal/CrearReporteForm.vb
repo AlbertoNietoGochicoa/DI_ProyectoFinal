4d12a8b33c35caa01ac299afe07faafd9c33ca97
Luptupulus <luptupulus@hotmail.com>
2016-05-23 12:24:52 +0200

    Retoques de reportes etc...

--- ProyectoFinal/ProyectoFinal/CrearReporteForm.vb
+++ ProyectoFinal/ProyectoFinal/CrearReporteForm.vb
@@ -1,6 +1,12 @@
 ﻿Public Class CrearReporteForm
     Private Sub CrearReporteForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.ReadOnly = True
+        TextBox2.ReadOnly = True
+        Button2.Enabled = False
+        GuardarReporBut.Enabled = False
+        MsgBox("Implementacion en fase de desarollo, funcionalidad desabilitada temporalmente.")
 
+        Me.Dispose()
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
